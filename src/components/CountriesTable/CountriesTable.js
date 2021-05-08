69e581633d1af1c9f3d7a3d004e699a0ae5d13ef
Hamza Ben Romdhane <hamza.ben.romdhane@artify.tn>
2021-05-08 17:55:40 +0100

    Init countries list with sort and search

---
+++ src/components/CountriesTable/CountriesTable.js
@@ -0,0 +1,57 @@
+import { KeyboardArrowDown, KeyboardArrowUp } from "@material-ui/icons";
+import { useState } from "react";
+import styles from "./CountriesTable.module.css";
+
+const orderBy = (countries, value, direction = 'asc') => {
+	return [...countries].sort((a, b) => a[value] > b[value] ? (direction == 'asc' ? 1 : -1): (direction == 'asc' ? -1 : 1));
+}
+
+const SortArrow = ({ direction }) => {
+	if (!direction) return <></>;
+	if (direction == 'asc') return <div className={styles.heading_arrow}><KeyboardArrowDown color="inherit" /></div>
+	else if (direction == 'desc') return <div className={styles.heading_arrow}><KeyboardArrowUp color="inherit" /></div>
+}
+
+const CountriesTable = ({ countries }) => {
+	const [direction, setDirection] = useState();
+	const [value, setValue] = useState();
+
+	const orderedCountries = orderBy(countries, value, direction);
+
+	const switchDirection = () => {
+		if (!direction) setDirection('desc');
+		else if (direction == 'desc') setDirection('asc');
+		else setDirection(null);
+	}
+
+	const setValueAndDirection = (value) => {
+		switchDirection();
+		setValue(value);
+	}
+
+	return <div>
+		<div className={styles.heading}>
+			<button
+				className={styles.heading_name}
+				onClick={() => setValueAndDirection('name')}>
+				<div>Name</div>
+				<SortArrow />		
+			</button>
+
+			<button
+				className={styles.heading_population}
+				onClick={() => setValueAndDirection('population')}>
+				<div>Population</div>
+				<SortArrow direction={direction} />		
+			</button>
+		</div>
+
+		{orderedCountries.map((country, index) => 
+		<div key={index} className={styles.row}>
+			<div className={styles.name}>{country.name}</div>
+			<div className={styles.population}>{country.population}</div>
+		</div>)}
+	</div>
+}
+
+export default CountriesTable;
---
+++ src/pages/index.js
@@ -0,0 +1,39 @@
+import Head from 'next/head'
+import { useState } from 'react';
+import CountriesTable from '../components/CountriesTable/CountriesTable';
+import Layout from '../components/Layout/Layout'
+import SearchInput from '../components/SearchInput/SearchInput'
+import styles from "../styles/Home.module.css";
+
+export default function Home({ countries }) {
+	const [keyword, setKeyword] = useState("");
+	const filterdCountries = countries.filter(_c =>
+		_c.name.toLowerCase().includes(keyword) || _c.region.toLowerCase().includes(keyword)
+		|| _c.subregion.toLowerCase().includes(keyword)
+	);
+
+	const onInputChange = (e) => {
+		e.preventDefault()
+		setKeyword(e.target.value.toLowerCase());
+	}
+
+	return <Layout>
+		<div className={styles.counts}>
+			Found {countries.length} countries
+		</div>
+		<SearchInput placeholder="Filter by name, region, subregion ..." onChange={onInputChange} />
+
+		<CountriesTable countries={filterdCountries} />
+	</Layout>
+}
+
+export const getStaticProps = async () => {
+	const res = await fetch("https://restcountries.eu/rest/v2/all");
+	const countries = await res.json();
+
+	return {
+		props: {
+			countries
+		}
+	}
+}
